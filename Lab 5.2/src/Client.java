5cd672f148637aa5efe62bac23c735d5093d4894
pnava589 <pnava589@B107-014.ad.mtroyal.ca>
2019-11-06 14:20:33 -0700

    Doing the loop for the input

--- Lab 5.2/src/Client.java
+++ Lab 5.2/src/Client.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 import java.net.*;
 
 
@@ -7,7 +8,9 @@ public class Client {
 	public static void main (String [] args) throws IOException
 	{
 		Socket s = new Socket("localhost",4100);
-		 PrintWriter pr = new PrintWriter(s.getOutputStream());
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		Scanner scan = new Scanner(System.in);
+		//String input = scan;
 		pr.println("GM, RU COMING B4 NOON? PLZ WB ASAP. TYVM. GTG, TTYL.");
 		pr.flush();
 		s.close();
